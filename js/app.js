21417bd099b2bdab3bd32068f611d758f622d34e
Biomilchtrinker <6145@htl.rennweg.at>
2020-02-24 09:24:59 +0100

    JS Error behoben

--- js/app.js
+++ js/app.js
@@ -22,22 +22,3 @@ const navSlide = () => {
 navSlide();
 // ---------------------
 
-// progress bars
-const progressbar1 = document.querySelector('.planung');
-const progressbar2 = document.querySelector('.durchfuehrung');
-const progressbar3 = document.querySelector('.testing');
-const progressbar1Mobile = document.querySelector('.planung-mobile');
-const progressbar2Mobile = document.querySelector('.durchfuehrung-mobile');
-const progressbar3Mobile = document.querySelector('.testing-mobile');
-
-window.onload = pre_loader;
-
-function pre_loader() {
-    let p1 = progressbar1.style.width = '397px';
-    let p2 = progressbar2.style.width = '300px';
-    let p3 = progressbar3.style.width = '140px';
-    let p1Mobile = progressbar1Mobile.style.width = '297px';
-    let p2Mobile = progressbar2Mobile.style.width = '225px';
-    let p3Mobile = progressbar3Mobile.style.width = '105px';
-}
-// ---------------------
