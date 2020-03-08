1cc3b6d192e089ac70089b132df82c19f0149f7b
Biomilchtrinker <6145@htl.rennweg.at>
2020-03-08 14:53:36 +0100

    neue Version

---
+++ js/3d.js
@@ -0,0 +1,18 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var loader = new THREE.GLTFLoader();
+
+loader.load( '../pictures/3D/autoschlüssel-kubesch.glb', function ( gltf ) {
+
+    scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
